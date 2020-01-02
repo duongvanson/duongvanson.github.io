6203cca20301069354f2c6600b010faf45c764f1
Son Duong <duongson.dev@gmail.com>
2020-01-02 17:39:27 +0700

    ver 1.0

---
+++ js/animations.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $("h1").slideDown(500);
+    $("#intro").show(1000);
+    $("#avatar").show(1000);
+    $(".line-info").slideDown(2000);
+    $("h2").show(1000);
+    $("#social").show(2000);
+});
